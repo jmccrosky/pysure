989e0e782b4b64c3c0278bf583fb80452212bab0
Jesse McCrosky <mccrosky@gmail.com>
2021-09-06 15:55:21 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pysure",
-    version="0.1.0",
+    version="0.1.1",
     description="Surrogate residuals for logistic regression",
     long_description=README,
     long_description_content_type="text/markdown",
